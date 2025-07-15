5c10f44446e21cdc9606ec98bb34513eeab5aa66
annnaisss14 <annais.zuniga.1402@gmail.com>
2025-07-15 14:30:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function verDetalle(ramo) {
+  const detalles = {
+    intro: "Este ramo introduce al estudiante en el campo profesional del Contador Auditor, sus ámbitos de acción y desafíos éticos."
+    // Agrega más detalles según necesites
+  };
+
+  document.getElementById("detalle").innerText = detalles[ramo] || "Información no disponible.";
+}
 
